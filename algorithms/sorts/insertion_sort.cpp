e3c7ef6182920fa7865447de779fe92830acda23
Jacob Gagne <jacob_gagne@hotmail.com>
2018-01-24 14:22:29 -0500

    pretty sure that was bubble sort

--- algorithms/sorts/insertion_sort.cpp
+++ algorithms/sorts/insertion_sort.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-void insertion_sort(int * numbers, const int size){
+void bubble_sort(int * numbers, const int size){
     int temp;
     for (int i = 1; i < size; i++) {
-        for(int j = i ; j > 0 ; j--){
+        for(int j = i; j > 0; j--){
             if(numbers[j] < numbers[j-1]){
                 temp = numbers[j];
                 numbers[j] = numbers[j-1];
@@ -18,7 +18,7 @@ int main(){
     int size = 7;
     int numbers[] = {900,3,5,1,4352642,34,329};
 
-    insertion_sort(numbers, size);
+    bubble_sort(numbers, size);
 
     for(int i = 0; i < size; i++){
         std::cout << numbers[i] << " ";
